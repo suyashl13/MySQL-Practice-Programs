d60626bdfaa3e370c4a421b84b9c2b9326cec7ba
Suyash Lawand <suyash.lawand@gmail.com>
2024-06-27 23:09:08 +0530

    add: CRUD Queries

---
+++ 03_basics_of_crud/CRUD_QUERIES.sql
@@ -0,0 +1,18 @@
+ -- Describe Cats Table
+DESC cats;
+
+-- Select All From Cats Table;
+SELECT * FROM Cats c ;
+
+-- Using WHERE clause
+SELECT * 
+FROM Cats c
+WHERE c.age > 3;
+
+-- Alter Table
+ALTER TABLE Cats 
+ADD COLUMN breed varchar(20) default 'Maine Coon' not null;
+
+-- Delete Table
+DELETE 
+FROM Cats 
